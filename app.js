fd14ebbfb7f562aed0f5caaa352765217788cc96
₳ɣēɦ <ayehmozaffari1402@gmail.com>
2023-08-15 23:34:11 +0330

    yhjk

---
+++ app.js
@@ -0,0 +1,5 @@
+class Result{
+    static whoWin(yourchoice, PCchoice){
+        if(yourchoice === "rock" && PCchoice ==="scissors" || yourchoice === "scissors"&& PCchoice == "paper" || yourchoice === )
+    }
+}
